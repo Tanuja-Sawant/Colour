1811aebbae62cdf571d4a4b16cea8a964050e6a5
Shade5 <f2013112@goa.bits-pilani.ac.in>
2016-08-09 16:54:57 +0530

    Added Circular Linked List

--- cirLink.c
+++ cirLink.c
@@ -20,30 +20,49 @@ struct Node *CircularLL(int data){
 	L->next=L;
 }
 
+void DeleteNode(struct Node **head,int index){
+
+	if(index==0)	
+	{
+
+	}
+	int i;
+	struct Node *temp = *head;
+	for(i=0;i<index-1;i++){
+		temp=temp->next;	
+	}
+
+	struct Node * temp2 = (temp)->next;
+	(temp)->next=(temp)->next->next;
+	free(temp2);
+
+
+}
+
 void InsertNode(struct Node **head,int data,int index){
 	if(index==0)	
 	{
 
 	}
 
+	int i;
 	struct Node *L=CreateNode(data);
-	int i=0;
+	struct Node *temp = *head;
 	for(i=0;i<index-1;i++){
-		*head=(*head)->next;
-		printf("%d",index);
+		temp=temp->next;	
 	}
-	L->next=(*head)->next;
-	(*head)->next=L;
+	L->next=temp->next;
+	temp->next=L;
 
 }
 
-int get(struct Node **head,int index){
+int get(struct Node *head,int index){
 
 	int i=0;
 	for(;i<index;i++){
-		*head=(*head)->next;
+		head=(head)->next;
 	}
-	return (*head)->data;
+	return (head)->data;
 
 }
 
@@ -60,14 +79,20 @@ int lsize(struct Node *head){
 
 int main(){
 	struct Node *L=CircularLL(10);
+
+	int i=0;
+	
+
 	
 	InsertNode(&L,3,1);
-	InsertNode(&L,30,1);
-	InsertNode(&L,30,1);
+	InsertNode(&L,4,1);
+	InsertNode(&L,5,3);
+	InsertNode(&L,6,3);
+	DeleteNode(&L,3);
 	
-	
-	
-	printf("%d",lsize(L));
+	for(i=0;i<lsize(L);i++){
+		printf("%d %d\n",get(L,i),i);
+	}
 
 
 	return 0;
