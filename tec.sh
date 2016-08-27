72ae990ae0741cc89c390a0d017bd47025280dae
Shade5 <f2013112@goa.bits-pilani.ac.in>
2016-08-27 15:20:00 +0530

    HHH

--- tec.sh
+++ tec.sh
@@ -1,3 +1,17 @@
 clear
-gcc cirLink.c 
-./a.out
+gcc process.c 
+par=0;
+
+for ((i=0;i<10;i++))
+do
+	d=$(./a.out);
+
+	if [ $d == 10 ]
+		then
+		par=$((par + 1 ))
+	fi
+
+
+done
+echo "parent execcutes first: "
+echo $par
