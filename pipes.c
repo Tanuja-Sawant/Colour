ade484527b6ac91860646aec0c09c8a69150c6dd
Tanuja-Sawant <thinkbigtanu@gmail.com>
2016-08-29 14:47:50 +0530

    Update pipes.c

--- pipes.c
+++ pipes.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char **argv){
 
-	int cpid; 
+	int cpid,gpid1,gpid2,gpid3; 
 	int status;
 	int fd[2];
     	int val = 0;
@@ -17,66 +17,80 @@ int main(int argc, char **argv){
 	cpid= fork();
 
 	if(cpid == 0){
-		cpid= fork();
-		if(cpid != 0){
-			cpid= fork();
-			if(cpid != 0){
-				cpid= fork();
-				if(cpid != 0){ // two
+		gpid1= fork();
+		if(gpid1 != 0){
+			gpid2= fork();
+			if(gpid2 != 0){
+				gpid3= fork();
+				if(gpid3 != 0){ // two
 					printf("I am %d Child is %d \n", getpid(),cpid);
 		
-					int w = waitpid(cpid, &status,WNOHANG);
+					int w=0,a,b,c;
 					while(w==0){
-						w = waitpid(cpid, &status,WNOHANG);
-						
-						// now read the data (will block)
-						read(fd[0], &val, sizeof(val));
-						printf("Parent(%d) received value: %d\n", getpid(), val);
 
-						// close the read-descriptor
-						close(fd[0]);
+						a = waitpid(gpid1, &status,WNOHANG);
+						b = waitpid(gpid2, &status,WNOHANG);
+						c = waitpid(gpid3, &status,WNOHANG);
+						w=(a&&b)||(b&&c)||(a&&c);
+						//printf("%d  \n",w);
+
+						
+						// send the value on the write-descriptor.
+						val = w;
+						write(fd[1], &val, sizeof(val));					
+						// close the write descriptor
+						
 					}
-					printf("Exited Now");
-					printf("Status %p \n",&status);
+					
+					//printf("Status %p \n",&status);
 				}
 				else{ // five
 
 
 					int i,j;
-					for(i=0;i<10000;i++){
+					for(i=0;i<90000;i++){
 						for(j=0;j<10000;j++){
 						}
 					}					
-					printf("I am %d Child is %d Excited\n", getpid(),cpid);
-					
-
-					close(fd[0]);
-					// send the value on the write-descriptor.
-					val = 100;
-					write(fd[1], &val, sizeof(val));					
-					// close the write descriptor
-					close(fd[1]);
-
-
+					//printf("I am %d Child is %d Excited\n", getpid(),cpid);
+					printf("Dead!\n");
 				}
 			}
 			else{ // four
-				printf("I am %d Child is %d \n", getpid(),cpid);
 
-					close(fd[0]);
-					// send the value on the write-descriptor.
-					val = 1000;
-					write(fd[1], &val, sizeof(val));					
-					// close the write descriptor
-					
+									int i,j;
+					for(i=0;i<50000;i++){
+						for(j=0;j<10000;j++){
+						}
+					}	
+
+				//printf("I am %d Child is %d \n", getpid(),cpid);					
+				printf("Dead!\n");
 			}
 		}
 		else{ // three
-			printf("I am %d Child is %d \n", getpid(),cpid);
+
+					int i,j;
+					for(i=0;i<100000;i++){
+						for(j=0;j<10000;j++){
+						}
+					}	
+
+			//printf("I am %d Child is %d \n", getpid(),cpid);
+			printf("Dead!\n");
 		}
 	}
 	else{ // one
-		printf("I am %d Child is %d \n", getpid(),cpid);
+		//printf("I am %d Child is %d \n", getpid(),cpid);
+		while(val!=1){
+			read(fd[0], &val, sizeof(val));
+			if(val)
+			printf("Two are dead\n");
+		// close the read-descriptor		
+		}
+		exit(0);
+
+
 	}
 		
 
@@ -84,4 +98,3 @@ int main(int argc, char **argv){
 return 0;
 
 }
-
